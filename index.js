f61ed3702e63b504f986fb8307a3965e4a2b2f63
vigneshwaran <vigneshvenketasan@gmail.com>
2025-10-29 21:50:37 +0530

    ready to commit

--- index.js
+++ index.js
@@ -1,42 +1,70 @@
-    // in this vedio we see star pattern in js
+  
+// JAVASCRIPT INTERVIEW QUESTIONS
 
-    // 1 Right - Angle Traingle Pattern
+  const javascriptInterviewQuestions = {
+    question1: {
+      title: "Is javascript a dynamically typed language or a statically typed language?",
+      answer: `Javascript is a dynamically typed language.
+  It means all type checks are done at run time when program is executing.
+  So, we can just assign anything to the variable and it works fine.
 
-    // let n = 5;
-    // for(let i =1; i<=n ; i++){
-    //     let stars = "";
-    //     for(let j =1 ; j<= i ; j++){
-    //         stars += "* "
-    //     }
-    //     console.log(stars)
-    // }
-     
+  Typescript is a statically typed language. All checks are performed at compile time.`,
+      example: `let a;
+        a = 0;
+        console.log(a); // 0
+        a = "Hello";
+        console.log(a); // "Hello"`
+    },
+    
+    question2: {
+      title: "What are the different datatypes in javascript?",
+      answer: {
+        primitiveDataTypes: [
+          "String",
+          "Number",
+          "Boolean",
+          "Null",
+          "Undefined",
+          "BigInt"
+        ],
+        nonPrimitiveDataTypes: [
+          "Object",
+          "Array",
+          "Date",
+          "functions"
+        ]
+      }
+    },
+    
+    question3: {
+      title: "What is Hoisting in javascript?",
+      answer: `In other scripting/server side languages, variables or functions must be declared before using it.
 
-    //2 Inverted right _Angle Triangle Pattern
+  In javascript, variables and functions can be used before declaring it. 
+  The javascript compiler moves all the declarations of variables and functions on top,
+   so there will not be any error. This is called hoisting.`
+    },
+    
+    question4: {
+      title: "What are the various things hoisted in javascript?",
+      answer: [
 
-    //       let n = 5;
-    // for(let i = n ; i >=1 ; i--){
-    //     let stars = "";
-    //     for(let j =1 ; j<= i ; j++){
-    //         stars += "* "
-    //     }
-    //     console.log(stars)
-    // }
-
-    // 3 pyramid Pattern
-
-    let n = 5;
-
-    for ( let i = 1; i <= n; i++){
-        let pattern = "";
-
-        for (let j= 1; j<=n-i; j++){
-            pattern += " "
-        }
-        // we create stars
-        for(let k=1; k<=i; k++){
-            pattern +="* "
-       }
-       console.log(pattern)
+        
+        "Function declarations: Fully hoisted.",
+        "var: Hoisted",
+        "Arrow functions: Not hoisted",
+        "Anonymous Function expressions: Not hoisted",
+        "let: Hoisted but not initialized. (Temporal dead zone).",
+        "const: Hoisted but not initialized. (Temporal dead zone)",
+        "class declarations: Hoisted but not initialized."
+      ]
+    },
+    
+    question5: {
+      title: "What is temporal dead zone?",
+      answer: `It is a specific time period in the execution of javascript
+      code where the variables declared with let and const exists but cannot 
+      be accessed until the value is assigned.
+  Any attempt to access them result in reference errors.`
     }
-         // thank you for watching guys +_
+  };
